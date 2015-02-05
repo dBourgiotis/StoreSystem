0c62d9c33169f6bc91916202cb1bab1014fa34e2
dimitrisMaf28 <dbourgiotis@gmail.com>
2015-02-05 17:00:03 +0200

    get and Put requests

--- src/serverstore/ServerStore.java
+++ src/serverstore/ServerStore.java
@@ -37,7 +37,8 @@ public class ServerStore {
         loadMaster();
         loadWorker();
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-        server.createContext("/store", new MyHandler());
+        server.createContext("/info", new MyHandler());
+        server.createContext("/store", new GetHandler());
         server.setExecutor(null); // creates a default executor
         server.start();
     }
@@ -46,7 +47,56 @@ public class ServerStore {
 
     static class MyHandler implements HttpHandler {
         public void handle(HttpExchange t) throws IOException {
-            String response = "Put a key to get its value or put a key and its value to store it";
+            String response = " Put a key like /store/key=yourKey to get its value or put a key and its value like this /store/key=yourKey&value=yourValue to store it";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+    
+    static class GetHandler implements HttpHandler {
+        public void handle(HttpExchange t) throws IOException {
+            //edw arxika 8a psaxnei a tou dinei mono key kai 8a emfanizei to value diaforetika 8a to apo8hkeuei se ena apo ta hashmaps
+            String response = "";
+            String uri = t.getRequestURI().toString();
+            String[] parts = uri.split("/");
+            if(parts.length>3)
+                response = "Wrong uri please go to /info for more information";
+            else{
+                if(parts[2].contains("&")){//if so, it is a put request
+                    String[] subParts = parts[2].split("&");
+                    if(subParts.length>2)
+                        response = "Wrong uri please go to /info for more information";
+                    else{
+                        String[] key = subParts[0].split("=");
+                        String[] value = subParts[1].split("=");
+                        if( key.length >2 || value.length > 2)
+                            response = "Wrong uri please go to /info for more information";
+                        else{
+                            response = "Key = "+key[1]+", Value = "+value[1] ;
+                            //*************************edw tha ginetai h apo8hkeush***********************
+                        }
+                    }
+                }
+                else if(parts[2].contains("=")){
+                    String[] subParts = parts[2].split("=");
+                    if(subParts.length>2)
+                        response = "Wrong uri please go to /info for more information";
+                    else{
+                        String[] key = subParts[0].split("=");
+                        if( key.length >2 )
+                            response = "Wrong uri please go to /info for more information";
+                        else{
+                            response = "Key = "+key[1] ;
+                            //*************************edw tha ginetai  to load***********************
+                        }
+                    }
+                }
+                else{
+                    response = "Wrong uri please go to /info for more information";
+                }                
+            }            
             t.sendResponseHeaders(200, response.length());
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
