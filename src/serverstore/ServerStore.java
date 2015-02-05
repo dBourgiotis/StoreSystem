f7536133e9aa0be018c856f93cfe165da3b28a2f
dimitrisMaf28 <dbourgiotis@gmail.com>
2015-02-05 12:51:14 +0200

    addKeyValue

--- src/serverstore/ServerStore.java
+++ src/serverstore/ServerStore.java
@@ -14,16 +14,17 @@ import java.net.InetSocketAddress;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import java.util.HashMap;
 /**
  *
  * @author dimbourgiotis
  */
 public class ServerStore {
 
-    //TODO hashmaps
-    /**
-     * @param args the command line arguments
-     */
+    private HashMap workerHas = new HashMap();//saves keys and values
+    private HashMap masterHas = new HashMap();//saves keys and keyLocations
+    private static int flag = 1;// for dividing save actions in all the workers
+    
      public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
         server.createContext("/store", new MyHandler());
@@ -41,6 +42,36 @@ public class ServerStore {
             os.write(response.getBytes());
             os.close();
         }
+    }
+    
+    public boolean addKeyValue(String key , String value){
+        if(masterHas.containsKey(key)){
+            return false;//this key is already saved
+        }else{
+            switch(flag){
+                case 1:{
+                    workerHas.put(key, value);
+                    masterHas.put(key, flag );// anti gia flag tha bei se string to Url
+                    flag = 2;
+                    return true;
+                }
+                case 2:{
+                    //connect to socket and send the key value
+                    masterHas.put(key, flag );// anti gia flag tha bei se string to Url
+                    flag = 3;
+                    return true;
+                }
+                case 3:{
+                    //connect to socket and send the key value
+                    masterHas.put(key, flag );// anti gia flag tha bei se string to Url
+                    flag = 1;
+                    return true;
+                }
+                default:{
+                    return false;
+                }                    
+            }                        
+        } 
     }
     
 }
